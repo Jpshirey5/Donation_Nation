dacd89374aa6a1d9ec5ef3d1b539df073a70157c
jpshirey5 <jayshirey@icloud.com>
2023-06-07 22:02:33 -0400

    Front-end Complete, needs functionality added

---
+++ Client/src/pages/createCharity.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+export default function Login() {
+
+  return (
+        <form className="signup-form">
+        <p className="title">Create your Charity </p>
+        <p className="message">Get started to share your project with the world! </p>
+            <div className="flex">
+        </div>  
+                
+        <label>
+            <input required="" placeholder="" type="text" className="input"></input>
+            <span>Charity Name</span>
+        </label>  
+
+        <label>
+            <input required="" placeholder="" type="text" className="input"></input>
+            <span>Description</span>
+        </label> 
+            
+        <label>
+            <input required="" placeholder="" type="text" className="input"></input>
+            <span>Goal</span>
+        </label>
+        <label>
+            <input required="" placeholder="insert hyperlink" type="text" className="input"></input>
+            <span>Stripe</span>
+        </label>
+        <button className="submit">Create</button>
+    </form>
+      )}
---
+++ Client/src/pages/submitThanks.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Footer from '../Components/footer';
+
+
+export default function Login() {
+  
+  return (
+    
+        <div className="homepage">
+          <div className='home-title'>
+            <h1>Thank you for signing up!</h1>
+            <p>Let's take some next steps!</p>
+          </div>
+          <Link to='/createCharity'><button className="btn-discover">
+              Create your own Charity
+            </button></Link>  
+          <div className="home-body">
+            <h1>Thanks for joining our Mission</h1>
+            <h4> Welcome to Donation Nation! When you become a member, you gain the incredible opportunity to create a meaningful impact in the lives of others. Joining our community of like-minded individuals allows you to support causes that truly resonate with your values and make a difference for those who are in need. With our user-friendly donation processes, personalized giving records, and valuable insights about charitable organizations, signing up gives you the power to amplify the effect of your contributions and bring about positive change in the world. We're thrilled to have you on board!
+            </h4>
+            <Link to='/donate'><button className="btn-donate">
+              Continue to Homepage
+            </button></Link>
+          </div>
+          <Footer />
+        </div>
+      )}
