504c2a90d39a733db8e9036401fc229fe32e35d1
jpshirey5 <jayshirey@icloud.com>
2023-06-08 20:41:44 -0400

    Front-end Done v2

---
+++ Client/src/pages/charityExpand.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default function Donate() {
+
+    return (
+
+        <div class="card">
+            <div class="content">
+                <p class="heading">Charity Name</p>
+                <p class="para">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi
+                    laboriosam at voluptas minus culpa deserunt delectus sapiente
+                    nventore pariatur
+                </p>
+                <Link to='/donate'><button className="btn-donate">
+                    Donate now
+                </button></Link>
+            </div>
+        </div>
+    )
+}
--- Client/src/pages/dashboard.js
+++ Client/src/pages/dashboard.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Footer from "../Components/footer";
+import { Link } from 'react-router-dom';
 
 export default function Dashboard() {
 
@@ -12,8 +13,8 @@ export default function Dashboard() {
           <div className="dash-container">
             <div className="column">
               <section className="donated-column">
-                <div className="donated-title">Previously Donated</div>
-                <div className="donated-history"></div>
+                <h3 className="donated-title">Previously Donated</h3>
+                <div className="donated-history">Compassion Cares $500</div>
               </section>
             </div>
 
@@ -23,7 +24,7 @@ export default function Dashboard() {
                 <p className="dash-card-text">
                   The Harmony Hope Foundation is dedicated to promoting unity and harmony within communities by providing resources and support to those in need. Through various programs and initiatives, the foundation aims to foster a sense of belonging, empower individuals, and create a positive impact on society. Whether it's offering educational opportunities, healthcare assistance, or community development projects, the Harmony Hope Foundation strives to create a harmonious and inclusive world for all.
                 </p>
-                <button className="donate-btn">More Info</button>
+                <Link to='/charityExpand'><button className="donate-btn">More Info</button></Link>
               </div>
 
               <div className="dash-card">
@@ -31,7 +32,7 @@ export default function Dashboard() {
                 <p className="dash-card-text">
                   Compassion Cares is a charity that focuses on spreading compassion and providing care to vulnerable populations. The organization's mission is to address the physical, emotional, and social needs of individuals and communities facing challenging circumstances. By offering support services, counseling, and outreach programs, Compassion Cares aims to alleviate suffering, restore hope, and promote resilience. With a strong emphasis on empathy and understanding, the charity strives to create a more compassionate and caring society.
                 </p>
-                <button className="donate-btn">More Info</button>
+                <Link to='/charityExpand'><button className="donate-btn">More Info</button></Link>
               </div>
 
               <div className="dash-card">
@@ -40,7 +41,7 @@ export default function Dashboard() {
                   The Bright Futures Initiative is dedicated to empowering underprivileged youth and providing them with opportunities for a brighter future. Through educational programs, mentorship, and skills development initiatives, the charity aims to break the cycle of poverty and create pathways for success. By equipping young individuals with the necessary tools and resources, the Bright Futures Initiative strives to unlock their potential, foster personal growth, and inspire them to become agents of positive change in their communities.
                 </p>
                 <div className="btns">
-                <button className="donate-btn">More Info</button>
+                <Link to='/charityExpand'><button className="donate-btn">More Info</button></Link>
                 </div>
               </div>
             </div>
