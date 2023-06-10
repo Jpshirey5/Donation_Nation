924fe9e67efe018c211022c63f02bacfcc7c1a91
Austin Nobregas <austinnobregas@gmail.com>
2023-06-10 19:51:32 -0400

    index.js in src re worked

--- Client/src/index.js
+++ Client/src/index.js
@@ -1,13 +1,45 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import 'bootstrap/dist/css/bootstrap.css';
+// import reportWebVitals from './reportWebVitals';
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//     <App />
+// );
+
+// reportWebVitals();
+
+
+import { ApolloClient, ApolloProvider } from '@apollo/client';
+import { InMemoryCache } from '@apollo/client';
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// Initialize ApolloClient
+const client = new ApolloClient({
+    uri: '/graphql',
+    cache: new InMemoryCache(),
+  });
+
+// injectStyles();
+
+// Find our rootElement or throw and error if it doesn't exist
+const rootElement = document.getElementById('root');
+if (!rootElement) throw new Error('Failed to find the root element');
+const root = ReactDOM.createRoot(rootElement);
+
+// Pass the ApolloClient instance to the ApolloProvider component
 root.render(
+  <ApolloProvider client={client}>
     <App />
+  </ApolloProvider>,
 );
 
 reportWebVitals();
