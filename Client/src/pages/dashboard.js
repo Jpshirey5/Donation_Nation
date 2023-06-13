d5b4df7751e7b89f07cd8b957eaea91747ea160e
Austin Nobregas <austinnobregas@gmail.com>
2023-06-12 20:23:50 -0400

    card populates from J.son

--- Client/src/pages/dashboard.js
+++ Client/src/pages/dashboard.js
@@ -34,7 +34,7 @@ export default function Dashboard() {
 
             <div>
               {charity.map((item) => (
-                <div key={item._id} className="card">
+                <div key={item._id} className="dash-card">
                   <h3 className="dash-card-title">{item.charityName}</h3>
                   <p className="dash-card-text">{item.description}</p>
                   <p>Goal: {item.goal}</p>
@@ -48,28 +48,3 @@ export default function Dashboard() {
     </div>
   )
 }
-{/* <div className="cards">
-<div className="dash-card">
-  <h2 className="dash-card-title">Harmony Hope Foundation</h2>
-  <p className="dash-card-text">
-    The Harmony Hope Foundation is dedicated to promoting unity and harmony within communities by providing resources and support to those in need. Through various programs and initiatives, the foundation aims to foster a sense of belonging, empower individuals, and create a positive impact on society. Whether it's offering educational opportunities, healthcare assistance, or community development projects, the Harmony Hope Foundation strives to create a harmonious and inclusive world for all.
-  </p>
-  <Link to='/charityExpand'><button className="donate-btn">More Info</button></Link>
-</div>
-
-<div className="dash-card">
-  <h2 className="dash-card-title">Compassion Cares</h2>
-  <p className="dash-card-text">
-    Compassion Cares is a charity that focuses on spreading compassion and providing care to vulnerable populations. The organization's mission is to address the physical, emotional, and social needs of individuals and communities facing challenging circumstances. By offering support services, counseling, and outreach programs, Compassion Cares aims to alleviate suffering, restore hope, and promote resilience. With a strong emphasis on empathy and understanding, the charity strives to create a more compassionate and caring society.
-  </p>
-  <Link to='/charityExpand'><button className="donate-btn">More Info</button></Link>
-</div> */}
-
-{/* <div className="cards">
-<div className="dash-card">
-  <h2 className="dash-card-title">Harmony Hope Foundation</h2>
-  <p className="dash-card-text">
-    The Harmony Hope Foundation is dedicated to promoting unity and harmony within communities by providing resources and support to those in need. Through various programs and initiatives, the foundation aims to foster a sense of belonging, empower individuals, and create a positive impact on society. Whether it's offering educational opportunities, healthcare assistance, or community development projects, the Harmony Hope Foundation strives to create a harmonious and inclusive world for all.
-  </p>
-  <Link to='/charityExpand'><button className="donate-btn">More Info</button></Link>
-</div> */}
